dea61fbcca71f03772dbac7d090ea04a7b295648
Brunoaqu <bruno.gl.aquino@gmail.com>
2023-11-21 15:28:02 -0300

    Dotfiles Bump 11/21/23 15h28

---
+++ .config/Code/User/History/-4b6273d1/xGAc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export TMUX="/tmp/tmux-tor"
+
+tmux start-server
+tmux -f ~/.config/tmux/tor.conf new-session -n "tor" "proxychains -q zsh"
---
+++ .config/Code/User/History/-5181fa84/6ql2.ts
@@ -0,0 +1,45 @@
+/* eslint-disable import/order */
+/* eslint-disable import/first */
+// import { setupTracing } from './tracer';
+
+// setupTracing('example-server');
+
+import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import helmet from 'helmet';
+import { middleware } from '.';
+import { log } from '../../utils/bunyan/log';
+import { v1Router } from './api/v1';
+import { isProduction } from '../../../config';
+
+const app = express();
+const origin = {
+  origin: '*',
+  exposedHeaders: ['cookie', 'x-auth-token'],
+};
+
+if (isProduction) {
+  app.use(middleware.rateLimit(15, 100));
+}
+
+app.disable('x-powered-by');
+app.enable('trust proxy');
+
+app.use('/v1', middleware.logRequest, v1Router);
+app.use(helmet());
+app.use(compression());
+app.use(cors(origin));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ extended: true, limit: '200mb' }));
+
+const serverOptions = { port: Number(process.env.API_PORT || 8080), host: 'localhost' };
+const httpLogger = log.child({
+  context: 'HttpServer',
+  'http.host': serverOptions.host,
+  'http.port': serverOptions.port,
+});
+
+app.listen(serverOptions.port, () => httpLogger.info(`Successfully started http server.`));
+
+export { app, httpLogger };
