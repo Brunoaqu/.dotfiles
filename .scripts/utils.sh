7153d61195401a610cc40447e17da7771d3c2d4f
Brunoaqu <bruno.gl.aquino@gmail.com>
2024-01-09 09:09:40 -0300

    Dotfiles Bump

--- .scripts/utils.sh
+++ .scripts/utils.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 if [ -z ${SUDO_USER} ]; then
-	    export USER_HOME="$HOME"
-    else
-	        export USER_HOME="/home/${SUDO_USER}"
+		export USER_HOME="$HOME"
+	else
+		export USER_HOME="/home/${SUDO_USER}"
 fi
 
 if lspci | grep -i NVIDIA &>/dev/null; then
-	    export NVIDIA_GPU=true
-    else
-	        export NVIDIA_GPU=false
+		export NVIDIA_GPU=true
+	else
+		export NVIDIA_GPU=false
 fi
